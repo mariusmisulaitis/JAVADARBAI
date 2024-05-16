c4709ef50742c3ba9a96367b18918ad30286e7c2
TechBar <marius.misulaitis7@gmail.com>
2024-05-16 22:49:09 +0300

    pageable example added

--- spring-example/src/main/java/lt/javadarbai/springexample/SpringExampleApplication.java
+++ spring-example/src/main/java/lt/javadarbai/springexample/SpringExampleApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @AllArgsConstructor
 @SpringBootApplication
@@ -22,7 +24,19 @@ public class SpringExampleApplication {
     @EventListener(ApplicationReadyEvent.class)
     public void runAfterStartup(){
         this.carService.addTestCars();
-        this.carService.printAllCars();
+//        this.carService.printAllCars();
+
+        this.carService.printAllCarsPageable(PageRequest.of(0, 2));
+//        this.carService.printAllCarsPageable(PageRequest.of(1, 2));
+        this.carService.printAllCarsPageable(PageRequest.of(2, 2));
+        this.carService.addTestCars();
+        this.carService.printAllCarsPageable(PageRequest.of(3, 2));
+        this.carService.printAllCarsPageable(PageRequest.of(4, 2));
+//        System.out.println();
+//        this.carService.printAllCarsByColor("GREEN");
+//        this.carService.printAllCarsByColor("RED");
+//        this.carService.printAllCarsByColor("BLUE");
+//        this.carService.test();
 
     }
 
--- spring-example/src/main/java/lt/javadarbai/springexample/repositories/CarRepository.java
+++ spring-example/src/main/java/lt/javadarbai/springexample/repositories/CarRepository.java
@@ -1,9 +1,33 @@
 package lt.javadarbai.springexample.repositories;
 
 import lt.javadarbai.springexample.entities.Car;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CarRepository extends JpaRepository<Car, Long> {
+
+    List<Car> getAllByColor(String color);
+
+    Boolean existsByColor(String color);
+
+    List<Car> getAllByColorOrModelOrMakeOrderByFuelType(String color, String model, String make);
+
+    Integer countAllByEngineSizeInLiters(Double engineSizeInLiters);
+
+    @Query("SELECT c FROM Car c WHERE c.color=?1")
+    List<Car> getCarsByColor(String color);
+
+    @Query("SELECT c FROM Car c WHERE c.color= :color OR c.model= :model OR c.make= :make")
+    List<Car> getCarsByColorOrModelOrMake(@Param("color") String color,
+                                          @Param("model") String model,
+                                          @Param("make") String make);
+
+    @Query(value = "SELECT * from car WHERE color= :color", nativeQuery = true)
+    List<Car> getCarsByColorNative(@Param("color") String color);
 }

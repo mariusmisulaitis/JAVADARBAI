eb6431c14462b55189425f08c23674a27333694c
TechBar <marius.misulaitis7@gmail.com>
2024-05-17 01:42:38 +0300

    added library project

---
+++ library/src/main/java/lt/javadarbai/library/LibraryApplication.java
@@ -0,0 +1,15 @@
+package lt.javadarbai.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryApplication {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello World!");
+		SpringApplication.run(LibraryApplication.class, args);
+
+	}
+}
